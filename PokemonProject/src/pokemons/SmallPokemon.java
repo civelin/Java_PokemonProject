2bf08e0bfd533137a0e0a3576145d1155f6ad398
civelin <cvetelinaivanova7@gmail.com>
2022-11-06 01:14:03 +0200

    committed package pokemon initial structure

--- PokemonProject/src/pokemons/SmallPokemon.java
+++ PokemonProject/src/pokemons/SmallPokemon.java
@@ -1,4 +1,19 @@
 package pokemons;
 
-public class SmallPokemon extends Pokemon{
+import java.util.List;
+
+public class SmallPokemon extends Pokemon {
+    public SmallPokemon(String name, List<String> types) {
+        this.name = name;
+        this.types = types;
+        this.hp = (int) (0.8 * Pokemon.defaultHp); // can do it because defaultHp is package private
+        this.attackPoints = (int) (0.8 * Pokemon.defaultAttackPoints);
+        this.defencePoints = (int) (0.8 * Pokemon.defaultDefencePoints);
+    }
+
+
+    @Override
+    public void revivePokemon() {
+        this.hp = (int) (0.8 * Pokemon.defaultHp);
+    }
 }
